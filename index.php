91ddc95dcb1aa841f8fbeec069c9b57bd25ded23
Alexander Shepherd <alexander.shepherd@gmail.com>
2014-02-07 21:29:39 +0000

    Index Page

--- index.php
+++ index.php
@@ -2,9 +2,29 @@
 
 include 'database.php';
 
-$elections = $db->query('SELECT * FROM election WHERE start < NOW() AND NOW() < end');
+$elections = $db->query
+(
+      'SELECT * FROM election '
+    . 'WHERE start < NOW() AND NOW() < end '
+    . ''
+);
+$user = 1;
 
 while ($election = $elections->fetch_assoc())
 {
-    var_dump($election);
+    $positions = $db->query
+    (
+          'SELECT * FROM position '
+        . 'WHERE id NOT IN '
+        . '('
+        .     'SELECT position_id FROM hasvoted '
+        .     'WHERE election_id=' . $election['id'] . ' '
+        .     'AND person_id=' . $user
+        . ')'
+    );
+
+    while ($position = $positions->fetch_assoc())
+    {
+        echo $position['name'];
+    }
 }
