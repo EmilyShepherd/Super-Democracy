b156daa5f0d437b9225404dc91d12be10d2f08a8
Christopher Baines <cb15g11@soton.ac.uk>
2014-02-08 12:00:12 +0000

    Some av stuff

--- election/index.php
+++ election/index.php
@@ -54,16 +54,125 @@ include '../common/header.php';
                 <?php
             }
         } else {
-            echo("AV");
+            $personVotes = array();
+            $personName = array();
+
+            $round = 1;
+
+            while (true) {
+
+                if ($round === 1) {
+                    $roundPrefs = $db->query
+                    (
+                        'SELECT COUNT(*) AS votes, person.name, vote_id, candidate_id FROM '
+                      . 'av, candidate, person WHERE av.candidate_id = candidate.id '
+                      . 'AND candidate.person_id = person.id AND '
+                      . 'candidate.position_id = ' . $position_id . ' AND '
+                      . 'candidate.election_id = ' . $election . ' AND '
+                      . 'pref = ' . ($round - 1) . ' GROUP BY candidate_id ORDER BY votes DESC'
+                    );
+
+                    echo($db->error);
+
+                    $roundPrefs = $roundPrefs->fetch_all(MYSQLI_ASSOC);
+
+                    if (count($roundPrefs) === 0) {
+                        echo("No candidates");
+                        break;
+                    }
+
+                    foreach ($roundPrefs as $candidate) {
+                        $personVotes[(int) $candidate['candidate_id']] = $candidate['votes'];
+                        $personName[(int) $candidate['candidate_id']] = $candidate['name'];
+                    }
+                } else {
+                    $roundPrefs = $db->query
+                    (
+                        'SELECT COUNT(*) AS votes, person.name, vote_id, candidate_id FROM '
+                      . 'av, candidate, person WHERE av.candidate_id = candidate.id '
+                      . 'AND candidate.person_id = person.id AND '
+                      . 'candidate.position_id = ' . $position_id . ' AND '
+                      . 'candidate.election_id = ' . $election . ' AND '
+                      . 'candidate_id = ' . $bottomCandidate . ' AND '
+                      . 'pref = ' . ($round - 1) . ' GROUP BY candidate_id ORDER BY votes DESC'
+                    );
+
+                    echo($db->error);
+
+                    $roundPrefs = $roundPrefs->fetch_all(MYSQLI_ASSOC);
+
+                    if (count($roundPrefs) == 1) {
+                        echo("Only one candidate remains");
+                        break;
+                    }
+                }
+
+                foreach ($personVotes as $id => $votes) {
+                    $name = $personName[$id];
+
+                    echo($name . " " . $votes . "<br>");
+                }
+
+                if (count($roundPrefs) === 1) {
+                    echo("No contest");
+                    break;
+                }
+
+                // Eliminate someone
+                $bottomCandidate = end(array_keys($personVotes));
+
+                echo('<br>' . $personName[$bottomCandidate] . ' is eliminated <br>');
+
+                unset($personName[$bottomCandidate]);
+                unset($personVotes[$bottomCandidate]);
+
+                $votesTransfered = false;
+                foreach ($roundPrefs as $vote) {
+                    $votes = (int) $candidate['votes'];
+                    $id = (int) $candidate['candidate_id'];
+
+                    if (!array_key_exists($id, $personVotes)) {
+                        continue;
+                    }
+
+                    $personVotes[$id] += $votes;
+
+                    echo($votes . " transfered to " . $personName[$id] . "<br>");
+
+                    $votesTransfered = true;
+                }
+
+                if (!$votesTransfered) {
+                    echo("<br>No votes transfered<br><br>");
+                }
+
+                foreach ($personVotes as $id => $votes) {
+                    $name = $personName[$id];
+
+                    echo($name . " " . $votes . "<br>");
+                }
+                echo("<br>");
+
+                // Check if the end conditions have been reached
+
+                $totalVotes = array_sum($personVotes);
+                $topVoted = array_keys($personVotes)[0];
+                $topVotedVotes = $personVotes[$topVoted];
+                arsort($personVotes);
+
+                if ($topVotedVotes > ($totalVotes / 2) + 1) {
+                    echo($personName[$topVoted] . " recieved more than half + 1 of the votes, so they are elected");
+                    break;
+                } else {
+                    echo($personName[$topVoted] . " did not recieve more than half + 1 of the votes, so the election goes to rounds");
+                }
+                echo("<br><br>");
+
+                $round++;
+            }
         }
-
     }
-
-
-
-
 ?>
-
     </div>
 <?php
 include '../common/footer.php';
