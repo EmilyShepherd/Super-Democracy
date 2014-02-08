f90a25a177214e462cd63fd59c3706e04f64c92e
Christopher Baines <cb15g11@soton.ac.uk>
2014-02-08 06:55:39 +0000

    Common header and footer for nominate.php

--- nominate.php
+++ nominate.php
@@ -1,70 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Super Democracy!</title>
+<?php
+    $title = "Nominate";
 
+    include 'common/header.php'
+?>
     <style>
       .pos_name
       {
         font-weight: bold;
       }
-      
+
       img.top
       {
         vertical-align:text-top;
       }
-      
+
       /* For large checkboxes, should we choose to use them */
       .large
       {
         width: 30px;
         height: 30px;
       }
-      
+
       div.button_list
       {
         padding-top:5px;
         padding-right:5px;
       }
-      
-      #delimiter
-      {
-        max-width:800px;
-        margin-left:auto;
-        margin-right:auto;
-      }
-      
+
       .manifesto
       {
         width:100%;
         min-height:100px;
       }
-      
+
       .points
       {
         width:50%;
         padding-bottom:5px;
       }
-      
+
       .points_field
       {
         width: 100%;
       }
     </style>
-    
-    <!-- Bootstrap -->
-    <link href="resources/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    
   </head>
   <body>
     <div id="delimiter">
@@ -73,7 +52,7 @@
       </header>
       <h2>Position</h2>
       <?php include 'model/index.php' ?>
-      
+
       <form action="addcandidate.php" method="post">
         <p>I wish to nominate myself for the position of: <select>
           <?php foreach ($positions as $position): ?>
@@ -103,15 +82,11 @@
           <input type="submit" class="btn btn-primary" name="submit" value="Nominate" />
         </div>
       </form>
-    
+
       <footer>
         <h4>Such copyright, many rights reserved</h3>
       </footer>
     </div>
-    
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://code.jquery.com/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="resources/bootstrap/dist/js/bootstrap.min.js"></script>
-  </body>
-</html>
+
+<?php
+    include 'common/footer.php';
